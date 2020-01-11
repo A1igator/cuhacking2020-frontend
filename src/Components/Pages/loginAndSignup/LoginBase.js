ac6a8627f4a68b866ba6cf5787738b37cf188048
Ali Abdoli <aliabdoli1970@gmail.com>
2020-01-11 14:16:09 -0500

    add logic for login and signup

--- src/Components/Pages/loginAndSignup/LoginBase.js
+++ src/Components/Pages/loginAndSignup/LoginBase.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { StyleSheet, Text, TextInput, Button, View } from 'react-native';
 
 export default function LoginBase(props) {
+  const [username, onChangeUsername] = useState('username');
+  const [password, onChangePassword] = useState('password');
+
   return (
     <View>
-      <TextInput>username</TextInput>
-      <TextInput>password</TextInput>
+      <TextInput onChangeText={text => onChangeUsername(text)} value={username} />
+      <TextInput onChangeText={text => onChangePassword(text)} value={password} />
       <Button title="Login" />
       <Button onPress={() =>
             props.navigation.navigate('SignupBase')
--- src/Components/Pages/loginAndSignup/SignupBase.js
+++ src/Components/Pages/loginAndSignup/SignupBase.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { StyleSheet, Text, TextInput, Button, View } from 'react-native';
 
 export default function SignupBase(props) {
+  const [username, onChangeUsername] = useState('username');
+  const [password, onChangePassword] = useState('password');
+  const [canSignup, setCanSignup] = useState(false);
+
   return (
     <View>
-      <TextInput>username</TextInput>
-      <TextInput>password</TextInput>
-      <TextInput>confirm password</TextInput>
+      <TextInput onChangeText={text => onChangeUsername(text)} value={username} />
+      <TextInput onChangeText={text => onChangePassword(text)} value={password} />
+      <TextInput onChangeText={text => {setCanSignup(text === password)}}>confirm password</TextInput>
       <Button title="Signup" />
       <Button onPress={() =>
             props.navigation.navigate('LoginBase')
