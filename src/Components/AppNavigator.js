b0a7b89b08400e5f3d39d5c4ef5d147f7441e223
404UsernameNotFound404 <2001henrymorris@gmail.com>
2020-01-11 14:44:28 -0500

    navigation

--- src/Components/AppNavigator.js
+++ src/Components/AppNavigator.js
@@ -2,10 +2,12 @@ import { createStackNavigator } from 'react-navigation-stack';
 import { createAppContainer } from 'react-navigation';
 import LoginBase from './Pages/loginAndSignup/LoginBase';
 import SignupBase from './Pages/loginAndSignup/SignupBase';
+import ProfilePage from './Pages/Profile/ProfilePage'
 
 const MainNavigator = createStackNavigator({
   LoginBase: { screen: LoginBase },
   SignupBase: { screen: SignupBase },
+  ProfilePage: {screen: ProfilePage }
 });
 
 const AppNavigator = createAppContainer(MainNavigator);
