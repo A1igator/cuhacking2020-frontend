bc1a0535daaa1a5ddb2cbbec0a33d6dab080fb56
404UsernameNotFound404 <2001henrymorris@gmail.com>
2020-01-11 13:37:31 -0500

    set up starting pages

---
+++ src/Components/Pages/Search/SearchPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, TextInput, Button, View } from 'react-native';
+
+export default function SearchPage() {
+  return (
+    <View>
+      <Text>Welcome to NAME____NAME</Text>
+      <Text>This is an app that allows students to help students.</Text>
+      <Button title = "Find A Tutor" />
+    </View>
+  );
+}
