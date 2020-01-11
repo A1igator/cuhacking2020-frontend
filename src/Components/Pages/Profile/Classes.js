cb3a8f5beb5fb7f0a8ee702aead5a4838ca38949
404UsernameNotFound404 <2001henrymorris@gmail.com>
2020-01-11 14:40:46 -0500

    stuff

--- src/Components/Pages/Profile/Classes.js
+++ src/Components/Pages/Profile/Classes.js
@@ -1,10 +1,30 @@
 import React from 'react';
-import { StyleSheet, Text, TextInput, Button, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function Classes(props) {
-  return (
-    <View>
-        {/* render all persons classes */}
-    </View>
-  );
+    const containerStyle = (colour) => {
+        return {
+            width: '50%',
+            backgroundColor: colour
+        }
+    }
+    
+    return (
+        <View style = {() => {containerStyle(props.colour)}}>
+            <Text>{props.name}</Text>
+            <Text>{props.catName}</Text>
+        </View>
+    );
 }
+
+
+
+const styles = StyleSheet.create({
+    container: {
+        width: '95%',
+    },
+    title: {
+        fontSize: 19,
+        fontWeight: 'bold',
+    }
+});
--- src/Components/Pages/Profile/ProfilePage.js
+++ src/Components/Pages/Profile/ProfilePage.js
@@ -1,14 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Classes from './Classes'
 import { StyleSheet, Text, TextInput, Button, View } from 'react-native';
 
 export default function ProfilePage(props) {
-  return (
-    <View>
-      <Text>Name: {props.name}</Text>
-      <Text>Location: {props.location}</Text>
-      <Text>Classes you can tutor:</Text>
-      <Classes />
-    </View>
-  );
+    const dummyDataCat = [
+        {name: 'Science', colour: 'red'},
+        {name: 'English', colour: 'green'},
+    ]
+    const [add, setAdd] = useState(false);
+    return (
+        <View style = {styles.container}>
+            <Text>Name: {props.name}</Text>
+            <Text>Location: {props.location}</Text>
+            <Text>Classes you can tutor</Text>
+            {
+                classes.map((ele) => {
+                    return <Classes name = {ele.name} colour = {ele.colour} catName = {ele.catName} />
+                })
+            }
+            <Button title="+" onPress = {add ? () => {setAdd(false)} : () => {setAdd(true)}} />
+            {add ?
+                <View>
+                    <TextInput>Name</TextInput>
+                    <Text>Pick A Category:</Text>
+                    {dummyDataCat.map((ele) => {
+                        return (
+                        <View colour = {ele.colour}>
+                            <Text>{ele.name}</Text>
+                        </View>)
+                    })}
+                </View>
+            : ''}
+        </View>
+    );
 }
+
+const styles = StyleSheet.create({
+    container: {
+      width: '95%',
+    },
+    title: {
+      fontSize: 19,
+      fontWeight: 'bold',
+    },
+    activeTitle: {
+      color: 'red',
+    },
+    categoryContainer: {
+        color: `${p => p.colour}`
+    }
+  });
